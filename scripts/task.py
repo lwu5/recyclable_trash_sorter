e4b5c8cbff0a734afd36d6f5db41ba75ac73dca3
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-05-24 13:09:13 -0500

    merged two files

--- scripts/task.py
+++ scripts/task.py
@@ -25,9 +25,7 @@ class InverseKinematics:
         # set up ROS / OpenCV bridge
         self.bridge = cv_bridge.CvBridge()
 
-        # initalize the debugging window
-        cv2.namedWindow("window", 1)
-
+        self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
 
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
 
@@ -79,77 +77,132 @@ class InverseKinematics:
         self.images = None
         self.scans = None
 
-        #control variables to keep track of the state the robot is in 
-        self.object_found = 0
-        self.gradient_descent_complete = 0
-        self.object_picked_up = 0
-        self.tag_found = 0
-        self.object_dropped = 0
-
-
         # indication to start moving towards an object or AR tag
         self.start_moving_forward = 0
 
         self.is_metal = 0 # 1 = metal
 
         self.object_list = [1, 2, 3]
+
+        # keep track of what color is found
+        self.detected_color = False
+
+        # keeps track of how far the object in the front is
+        self.front_distance = 1.0
+
+        # what the image width is
+        self.img_width = 100
+
+        # finding object (0) -> orient facing object (1) -> go towards object (2)
+        self.robot_state = 0
+
+        # state variable to check if it has rotated and adjusted itself towards the object
+        self.rotated = False
+
+        # lower and upper bounds for blue, pink, and green
+        self.color_dict = {
+            0: (np.array([95, 90, 100], np.array([105, 110, 150]))),
+            1: (np.array([155, 140, 120]), np.array([165, 160, 170])),
+            2: (np.array([30, 130, 90]), np.array([40, 150, 150]))
+        }
+
+        self.cx = 0
+        self.cy = 0
+        self.w = 0
         
         print('finished initializing!')
 
     def image_callback(self, img):
         self.images = img
+        # loads the image and checks for color in image
+        image = self.bridge.imgmsg_to_cv2(self.images,desired_encoding='bgr8') # loading the color image
+        assert image is not None
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        #dimensions of the image, used later for proportional control 
+        h, self.w, d = image.shape
+
+        if self.robot_state == 0:
+            for color in self.color_dict:
+                self.find_color(hsv, image, color)
         
     def scan_callback(self, data):
         self.scans = data.ranges
-    
-    def find_object(self):
-        # TODO: from scan and image data, find the end position that we want the robot arm to move to
-        if True:
-            self.goal_location = [0.3,0.1,0.3]
-            self.object_found = 1
+        self.front_distance = np.mean([data.ranges[i] for i in [0, 1, 2, 359, 358]])
+        if self.robot_state == 1:
+            self.rotate_towards_object()
+        elif self.robot_state == 2:
+            self.move_to_object()
+
+    def find_color(self, hsv: np.ndarray, img: np.ndarray, color: int) -> None:
+        # erases all the pixels in the image that aren't in that range
+        lower_bound, upper_bound = self.color_dict[color]
+        mask = cv2.inRange(hsv, lower_bound , upper_bound)
+
+        # determines the center of the colored pixels
+        M = cv2.connectedComponentsWithStats(mask)
+
+        # if it detected the color
+        if M['m00'] > 0:
+
+            self.detected_color = True
+            # center of the colored pixels in the image
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            self.cx = cx
+            self.cy = cy
+            # a red circle is visualized in the debugging window to indicate
+            # the center point of the colored pixels
+            cv2.circle(img, (cx, cy), 20, (0,0,255), -1)
+
+        else:
+            self.detected_color = False
+        
+        cv2.imshow("window", img)
+        cv2.waitKey(3)
+
+    def update_state(self):
+        if self.robot_state == 0 and self.detected_color:
+            self.robot_state = 1
+        elif self.robot_state == 1 and self.rotated:
+            self.robot_state = 2
+        elif self.robot_state == 2:
+            self.robot_state = 3
+        elif self.robot_state == 3: # include finding the goal location as well
+            self.pick_up_object()
+        elif self.robot_state == 4:
+            self.find_tag()
+        elif self.robot_state == 5:
+            self.drop_object()
+        else:
+            self.detected_color = False
+            self.rotated = False
+            self.robot_state = 0
+
+    def rotate_towards_object(self):
+        #TODO: ROTATE OBJECT AND MAKE SURE ITS FACING OBJECT
+        # proportional control to orient towards the colored object
+        angular_error = ((self.w/2) - (self.cx))
+        angular_k = 0.001
+        while abs(angular_error) < 0.02:
+            lin = Vector3(0, 0, 0)
+            ang = Vector3(0, 0, angular_k * angular_error)
+            twist = Twist(linear=lin, angular=ang)
+            self.vel_pub.publish(twist)
         return
 
-        if self.images is not None and self.scans is not None:
-            # setting the angular z value parameters to explore the environment in the angular direction
-            self.movement.angular.z = 0.2
-            image = self.bridge.imgmsg_to_cv2(self.images,desired_encoding='bgr8') # loading the color image
-            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            
-            #TODO: change this so that it's referring to objects later
-            color = 'pink'
-            #setting up the hsv ranges for the different colored objects 
-            if color == 'blue':
-                lower_bound = numpy.array([95, 90, 100]) 
-                upper_bound = numpy.array([105, 110, 150]) 
-            elif color == 'pink':
-                lower_bound= numpy.array([155, 140, 120]) 
-                upper_bound= numpy.array([165, 160, 170]) 
-            elif color == 'green':
-                lower_bound = numpy.array([30, 130, 90]) 
-                upper_bound = numpy.array([40, 150, 150]) 
-            
-            # erases all the pixels in the image that aren't in that range
-            mask = cv2.inRange(hsv, lower_bound, upper_bound)
-            
-            # determines the center of the colored pixels
-            M = cv2.moments(mask)
+    def move_to_object(self):
+        if self.scans is not None:
+            # go in front of object
+            print("Robot mover: Approaching object")
+            r = rospy.Rate(10)
+            while not abs(self.front_distance - 0.15) < 0.02:
+                lin = Vector3(min((self.front_distance - 0.15) * 0.4, 0.5), 0.0, 0.0)
+                ang = Vector3(0.0, 0.0, (self.img_width) * 0.01)
+                twist = Twist(linear=lin, angular=ang)
+                self.vel_pub.publish(twist)
+            self.goal_location = [0.3, 0, 0.2]
 
-            #dimensions of the image, used later for proportional control 
-            h, w, d = image.shape
-
-
-            # if it detected the color
-            if M['m00'] > 0:
-                # center of the colored pixels in the image
-                cx = int(M['m10']/M['m00'])
-                cy = int(M['m01']/M['m00'])
-                
-                # a red circle is visualized in the debugging window to indicate
-                # the center point of the colored pixels
-                cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-                # proportional control to orient towards the colored object
-            cv2.imshow("window", image)
-            cv2.waitKey(3)
     def find_tag(self):
         # Now that the robot has picked up the object, this is the method to find the correct AR tag 
         # using the aruco AR tag library. A numerical parameter (tag, can be 1, 2, or 3) specifies which
@@ -246,6 +299,7 @@ class InverseKinematics:
                                     self.tag_found = 1
                                     self.movement.linear.x = 0
                                     self.movement.angular.z = 0
+                                    self.robot_state = 5
                     else:
                         self.movement.linear.x = 0.0
             # to visualize the robot localizing to tags         
@@ -278,42 +332,21 @@ class InverseKinematics:
         l_2 = .130 # distance from second joint to third joint (m)
         l_3 = .124 # distance from third joint to fourth joint (m)
         l_4 = .126 # distance from fourth joint to end effector (m)
-        #l_1 = 0.077
-        #l_2 = 0.128
-        #l_3 = 0.024 + 0.124
-        #l_4 = 0.126
-        #j_1 = -1 * angles[0]
-        #j_2 = -1 * (angles[1] - block - math.radians(90))
-        #j_3 = -1 * (angles[2] - j_2)
-        #j_4 = -1 * (angles[3])
+
         # joint angles to input into forward kinematics equations
         j_1 = -1*(angles[0])
         j_2 = math.radians(90) - (angles[1] + block)
         j_3 = -1*(angles[2] + math.radians(90))
         j_4 = -1*(angles[3])
 
-        print('the adjusted angles', [j_1, j_2, j_3, j_4])
-        #curr_x = (l_2 * math.cos(j_1) * math.cos(j_2)) + (l_3 * math.cos(j_1) * math.cos(j_2 + j_3))
-        #curr_y = (l_2 * math.sin(j_1) * math.cos(j_2)) + (l_3 * math.sin(j_1) * math.cos(j_2 + j_3))
-        #curr_z = l_1 + (l_2*math.sin(j_2)) + (l_3*math.sin(j_2 + j_3))
         curr_x = math.cos(j_1)*(l_2*math.cos(j_2)+l_3*math.cos(j_2 + j_3)) + l_4*math.cos(j_1)*math.cos(j_2+j_3+j_4)
         curr_y = math.sin(j_1)*(l_2*math.cos(j_2)+l_3*math.cos(j_2 + j_3)) + l_4*math.sin(j_1)*math.cos(j_2+j_3+j_4)
         curr_z = l_1 + l_2*math.sin(j_2) + l_3*math.sin(j_2+j_3) + l_4*math.sin(j_2+j_3+j_4)
-        #print('first term', l_1)
-        #print('second_term', l_2*math.sin(j_2))
-        #print('third_term', l_3*math.sin(j_2+j_3))
-        #print('fourth_term', l_4*math.sin(j_2+j_3+j_4))
         # need to adjust for the height of the turtlebot; .03 is the measurement of the block between the first joint and the turtlebot height.
         #print('curr_z before adding tb_height', curr_z)
         curr_z += 0.035 + turtlebot_height # to offset for the problematic angle
-        #print('curr_z after adding tb height', curr_z)
 
-        #print('first term', math.cos(j_1)*(l_2*math.cos(j_2)))
-        #print('second_term', math.cos(j_1)*(l_3*math.cos(j_2+j_3)))
-        #print('third_term', math.cos(j_1)*(l_4*math.cos(j_2+j_3+j_4)))
-        #print('offset', curr_x - .195)
         curr_x -= 0.075 # to offset for the fact that x = 0 is around the center of the lidar scanner
-        #print('curr_x after accounting for tbot front edge', curr_x)
         
         # the curr_y is flipped in the robot arm coordinate system, 
         # need to account for that
@@ -405,6 +438,7 @@ class InverseKinematics:
         #self.do_gradient_descent(self.current_joint_angles, [0.1,0,0.4]) # also takes into account picking up object, specify an xyz
 
         self.object_picked_up = 1
+        self.robot_state = 4
 
 
     def drop_object(self):
@@ -436,33 +470,15 @@ class InverseKinematics:
         rospy.sleep(1)
         #updating control variables 
         self.object_dropped = 1
+        self.robot_state = 6
 
     def run(self):
         # Uses gradient descent to compute how much to move arms.
         # reference: https://www.alanzucconi.com/2017/04/10/robotic-arms/
-        rospy.sleep(3)
-        
-        while len(self.object_list) > 0: 
-            if not self.object_found: 
-                self.find_object()
-            elif not self.object_picked_up: # include finding the goal location as well
-                self.pick_up_object()
-            elif not self.tag_found:
-                self.find_tag()
-            elif not self.object_dropped:
-                self.drop_object()
-            # popping off the list of remaining actions to take once the action has been completed,
-            # resetting state variables
-            else:
-                self.object_list.pop(0)
-                self.object_found = 0
-                self.object_picked_up = 0
-                self.tag_found = 0
-                self.object_dropped = 0
-            rospy.sleep(1)
 
-        rospy.spin()
-
+        while True:
+            self.update_state()
 if __name__ == "__main__":
     node = InverseKinematics()
+    rospy.sleep(3)
     node.run()
